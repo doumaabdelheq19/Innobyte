066f3b78b85f2816452904179f4e7e1c841fc617
doumaabdelheq19 <104992323+doumaabdelheq19@users.noreply.github.com>
2023-12-16 11:21:40 +0100

    commit

---
+++ chatbot/app.js
@@ -0,0 +1,45 @@
+const form = document.getElementById("chat-form");
+const input = document.getElementById("chat-input");
+const messages = document.getElementById("chat-messages");
+const apiKey = "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";
+
+
+
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const message = input.value;
+  input.value = "";
+
+  messages.innerHTML += `<div class="message user-message">
+  <img src="Frame34.png" alt="user icon"> <span>${message}</span>
+  </div>`;
+
+  // Use axios library to make a POST request to the OpenAI API
+  const response = await axios.post(
+    "https://api.openai.com/v1/completions",
+    {
+      prompt: message,
+      model: "text-davinci-003",
+ 
+      temperature: 1,
+      max_tokens: 1000,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+     
+  
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+    }
+  );
+  const chatbotResponse = response.data.choices[0].text;
+
+  messages.innerHTML += `<div class="message bot-message">
+  <img src="robot1.png" alt="bot icon"> <span>${chatbotResponse}</span>
+  </div>`;
+});
